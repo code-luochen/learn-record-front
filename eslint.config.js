3b13181110aefc4495c5fbbb36b81085ffad85d1
luochun <2591451512@qq.com>
2025-01-13 10:08:56 +0800

    lintstaged

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
 export default [
-	js.configs.recommended,
-	{
-		ignores: ['node_modules'],
-		rules: {
-			semi: 0,
-			'prefer-const': 'error',
-		},
-	},
+	// js.configs.recommended,
+	// {
+	// 	ignores: ['node_modules'],
+	// 	rules: {
+	// 		semi: 0,
+	// 		'prefer-const': 'error',
+	// 	},
+	// },
 ]
