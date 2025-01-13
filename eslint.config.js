c0f56bdb2a3b9ad73a4e6134bbb969b1ae58d1a3
luochun <2591451512@qq.com>
2025-01-13 16:59:47 +0800

    style: init

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,6 @@
 import vueEslintParser from 'vue-eslint-parser'
 
 export default [
-	// js.configs.recommended,
-	// {
-	// 	ignores: ['node_modules'],
-	// 	rules: {
-	// 		semi: 0,
-	// 		'prefer-const': 'error',
-	// 	},
-	// },
 	{
 		files: ['./**/*.vue'],
 		languageOptions: {
